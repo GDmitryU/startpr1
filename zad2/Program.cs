1d87bb924e2f73ab71147570254733211d5ee8ec
DmitryG <dmitrygribchenko@mail.ru>
2022-08-29 22:56:16 +0300

    zad1.1

--- zad2/Program.cs
+++ zad2/Program.cs
@@ -2,7 +2,15 @@
 int num1 = Convert.ToInt32 (Console.ReadLine());
 Console.Write($"Введите число 2: ");
 int num2 = Convert.ToInt32 (Console.ReadLine());
-if (num2 > num1) {Console.WriteLine($"max = {num2}")}
-if (num1 > num2) {Console.WriteLine($"max = {num1}")}
-else {Console.WriteLine($"Неверно введены значения, или значения равны");}
-
+if (num2 > num1) 
+{
+    Console.WriteLine($"max = {num2}");
+}
+if (num1 > num2) 
+{
+    Console.WriteLine($"max = {num1}");
+}
+else 
+{
+    Console.WriteLine($"Неверно введены значения, или значения равны");
+}
