0689f3331f7ed297aa8f5cb841db5af108bd1589
DmitryG <dmitrygribchenko@mail.ru>
2022-08-30 00:09:08 +0300

    zad6

---
+++ zad6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число : ");
+double num = int.Parse(Console.ReadLine());
+while (num >= 2 || num <= -2)
+{
+    num = num / 2; 
+}
+Console.WriteLine($"Введено четное число ? ");
+if(num==1 || num==-1)
+{
+    Console.WriteLine($"да ");
+}
+else
+{
+    Console.WriteLine($"нет ");
+}
